56165e4ab1a26dd930c1dfc8c80d45eb6a98c68d
mikeyy2 <adhikari.shrish1@gmail.com>
2023-08-26 10:52:30 +0545

    Added simple kafka producer

---
+++ simple_producer.py
@@ -0,0 +1,27 @@
+import os
+
+from confluent_kafka import KafkaException, Producer
+from dotenv import load_dotenv
+
+def main():
+    settings = {
+        'bootstrap.servers': os.getenv('KAFKA_BOOTSTRAP_SERVERS'),
+        'security.protocol': os.getenv('KAFKA_SECURITY_PROTOCOL'),
+        'sasl.mechanisms': os.getenv('KAFKA_SASL_MECHANISMS', "PLAIN"),
+        'sasl.username': os.getenv('KAFKA_SASL_USERNAME'),
+        'sasl.password': os.getenv('KAFKA_SASL_PASSWORD'),
+    }
+    
+    producer = Producer(settings)
+    producer.produce(
+        topic="MeineKafkaTopic",
+        key=None,
+        value="MeineKafkaTopic-111"
+    )
+    # Wait for the confirmation that the message was received
+    producer.finish()
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    main()
