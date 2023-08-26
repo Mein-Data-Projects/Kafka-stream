027c4d65e79fb29049371c1c5d5a73cc70cf1bdf
mikeyy2 <adhikari.shrish1@gmail.com>
2023-08-26 22:07:57 +0545

    Added docs for kafka producer

--- kafka_producer.py
+++ kafka_producer.py
@@ -15,8 +15,13 @@ class KafkaProducer:
     def produce(self, message: ProducerMessage):
         try:
             self._producer.produce(message.topic, key=message.key, value=message.value)
+            # makes all buffered records immediately available to send
+            # will block until the requests associated with sending these
+            # records are completed
             self._producer.flush()
         except KafkaException as exc:
+            # skip over the message if it's too large for the Kafka cluster
+            # to consume
             if exc.args[0].code() == KafkaError.MSG_SIZE_TOO_LARGE:
                 pass # Handle the error here
             else:
